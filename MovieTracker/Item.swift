164adef0cdf8f06fe253b7b586cc8c7e079faa8a
Tobias Fu <fuz.sh0710@gmail.com>
2026-01-10 23:12:26 -1000

    Initial Commit

---
+++ MovieTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MovieTracker
+//
+//  Created by Tobias Fu on 1/10/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
