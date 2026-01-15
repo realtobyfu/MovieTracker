108034c8bcb4e826153648dee97cc6684ada595a
Tobias Fu <fuz.sh0710@gmail.com>
2026-01-14 15:33:55 -1000

    De-duplication + enum  for search modes

--- MovieTracker/MovieListView.swift
+++ MovieTracker/MovieListView.swift
@@ -7,14 +7,25 @@
 
 import SwiftUI
 
+enum Mode: Equatable {
+    case discover
+    case search(searchText: String)
+}
+
 struct MovieListView: View {
+    
+    private var service = MovieService()
+    
+    @State private var mode: Mode = .discover
+    @State private var searchText: String = ""
     
     @State private var isLoading: Bool = false
     @State private var movies = [Movie]()
-    @State private var searchText: String = ""
     @State private var errorMessage: String?
     @State private var currentPage: Int = 1
     @State private var totalPages: Int?
+    
+    @State private var loadedIDs = Set<Int>()
     
     var body: some View {
         NavigationStack {
@@ -30,11 +41,7 @@ struct MovieListView: View {
                         ProgressView()
                         .task {
                             currentPage += 1
-                            if searchText == "" {
-                                try? await fetchMovies()
-                            } else {
-                                try? await searchMovies(text: searchText)
-                            }
+                            try? await loadPage(mode: mode, page: currentPage)
                         }
                     }
                 }
@@ -46,101 +53,45 @@ struct MovieListView: View {
         .navigationTitle("Movies")
         .searchable(text: $searchText, prompt: "Search Movies")
         .task(id: searchText) {
+            if !searchText.isEmpty {
+                mode = .search(searchText: searchText)
+            } else {
+                mode = .discover
+            }
+
+            print(".task(id: mode) is triggered")
             isLoading = true
             defer { isLoading = false }
             do {
-                try await searchMovies(text: searchText)
+//                if currentPage == 1 {
+                try await resetAndLoad(mode: mode)
+                    
             } catch {
                 errorMessage = error.localizedDescription
                 movies = []
             }
         }
-        .task () {
-            do {
-                try await fetchMovies()
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
     }
     
-    func searchMovies(text: String) async throws {
-        
-        if (totalPages != nil) {
-            guard currentPage <= totalPages! else { return }
-        }
-
-        var comps = URLComponents(string: "https://api.themoviedb.org/3/search/movie")!
-        
-        comps.queryItems = [
-            URLQueryItem(name: "query", value: text),
-            URLQueryItem(name: "include_adult", value: "false"),
-            URLQueryItem(name: "language", value: "en-US"),
-            URLQueryItem(name: "page", value: String(currentPage))
-        ]
-        
-
-        var request = URLRequest(url: comps.url!)
-        request.httpMethod = "GET"
-        request.timeoutInterval = 10
-        request.allHTTPHeaderFields = [
-          "accept": "application/json",
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0YjMyYWIzN2U1MDg2ZmU1YzA5NTIxYzBlNjdhNyIsInN1YiI6IjU2NTYzOTFlOTI1MTQxMDllODAwMDMyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CnjFlQK6eKafVglC_aN3jx98dn9TD_SulgMz86RGohw"
-        ]
-
-        let (data, _) = try await URLSession.shared.data(for: request)
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        decoder.dateDecodingStrategy = .formatted(formatter)
-
-        let response = try decoder.decode(Response.self, from: data)
-        if currentPage == 1 {
-            movies = response.results
-        } else {
-            movies.append(contentsOf: response.results)
-        }
-        totalPages = response.totalPages
-        print("Searched \(text), repsonse: \(movies)")
-    }
-    
-    
-    
-    func fetchMovies() async throws {
-        
-        if (totalPages != nil) {
-            guard currentPage <= totalPages! else { return }
-        }
-        var comps = URLComponents(string: "https://api.themoviedb.org/3/discover/movie")
-        
-        comps?.queryItems = [
-            URLQueryItem(name: "include_adult", value: "false"),
-            URLQueryItem(name: "language", value: "en-US"),
-            URLQueryItem(name: "page", value: "\(currentPage)")
-        ]
-        
-        // create the URL
-        guard let url = comps?.url else { throw URLError(.badURL) }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("Bearer \(Constants.apiKey)", forHTTPHeaderField: "Authorization")
-        
-        
-        let (data, _) = try await URLSession.shared.data(for: request)
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        decoder.dateDecodingStrategy = .formatted(formatter)
-        let response = try? decoder.decode(Response.self, from: data)
-        
-        movies.append(contentsOf: response?.results ?? [])
+    func loadPage(mode: Mode, page: Int) async throws {
+        let response = try? await service.performRequest(mode: mode, page: page)
+        let newMovies: [Movie] = (response?.results.filter{ loadedIDs.insert($0.id).inserted }) ?? []
+        movies.append(contentsOf: newMovies)
         totalPages = response?.totalPages
     }
+    
+    
+    
+    func resetAndLoad(mode: Mode) async throws {
+        currentPage = 1
+        loadedIDs.removeAll()
+        movies.removeAll()
+        let response = try? await service.performRequest(mode: mode, page: currentPage)
+        let newMovies: [Movie] = (response?.results.filter{ loadedIDs.insert($0.id).inserted }) ?? []
+        movies.append(contentsOf: newMovies)
+        totalPages = response?.totalPages
+    }
+
 }
 
 
--- MovieTracker/MovieService.swift
+++ MovieTracker/MovieService.swift
@@ -1,95 +1,54 @@
-////
-////  MovieService.swift
-////  MovieTracker
-////
-////  Created by Tobias Fu on 1/10/26.
-////
-//
-//import Foundation
-//
-//enum NetworkError: Error {
-//    case invalidURL(url: String)
-//    case invalidResponse
-//    case decodingError(Error)
-//}
-//
-//
-//enum MovieEndpoint {
-//    case discover
-//    case search
-//    case detail(Int)
-//    
-//    var path: String {
-//        switch self {
-//        case .discover:
-//            return "/discover/movie"
-//        case .search:
-//            return "/search/movie"
-//        case .detail(let id):
-//            return "/movie/\(id)"
-//        }
-//    }
-//    
-//    func baseQueryItems() -> [URLQueryItem] {
-//        let commonItems = [
-//            URLQueryItem(name: "include_adult", value: "false"),
-//            URLQueryItem(name: "language", value: "en-US")
-//        ]
-//        
-//        switch self {
-//        case .discover:
-//            return commonItems + [
-//                URLQueryItem(name: "include_video", value: "false"),
-//                URLQueryItem(name: "sort_by", value: "popularity.desc")
-//            ]
-//        case .search, .detail:
-//            return commonItems
-//        }
-//        
-//    }
-//}
-//
-//struct MovieService {
-//    private let session: URLSession
-//    private let bearerToken: String
-//    private let baseURL: String
-//    private let decoder: JSONDecoder
-//    
-//    init(session: URLSession, bearerToken: String, baseURL: String, decoder: JSONDecoder) {
-//        self.session = session
-//        self.bearerToken = bearerToken
-//        self.baseURL = baseURL
-//        self.decoder = decoder
-//    }
-//    
-//    private func performRequest<T: Codable>(
-//        endpoint: MovieEndpoint,
-//        additionalQueryItems: [URLQueryItem] = [],
-//        responseType: T.Type
-//    ) async throws -> T {
-//        guard let url = URL(string: baseURL + endpoint.path) else {
-//            throw URLError(.badURL)
-//        }
-//        
-//        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-//            throw URLError(.badURL)
-//        }
-//        
-//        let queryItems = endpoint.baseQueryItems() + additionalQueryItems
-//        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
-//        
-//        var request = URLRequest(url: components.url!)
-//        request.httpMethod = "GET"
-//        request.timeoutInterval = 10
-//        request.allHTTPHeaderFields = [
-//            "accept":  "application/json",
-//            "Authorization": "Bearer \(bearerToken)"
-//        ]
-//        
-//        let (data, response) = try await session.data(for: request)
-//    }
-//        
-//    
-//    
-//    func fetchMovies(endpoint: MovieEndpoint, page: Int = 1, query: String? = nil) async throws -> [Movie] 
-//}
+///
+///  MovieService.swift
+///  MovieTracker
+///
+///  Created by Tobias Fu on 1/10/26.
+///
+
+import Foundation
+
+struct MovieService {
+    func performRequest(mode: Mode, page: Int) async throws -> Response {
+        var comps: URLComponents?
+        switch mode {
+        case .search(let searchText):
+            comps = URLComponents(string: "https://api.themoviedb.org/3/search/movie")!
+            comps?.queryItems = [
+                URLQueryItem(name: "query", value: searchText),
+                URLQueryItem(name: "include_adult", value: "false"),
+                URLQueryItem(name: "language", value: "en-US"),
+                URLQueryItem(name: "page", value: String(page))
+            ]
+        case .discover:
+            comps = URLComponents(string: "https://api.themoviedb.org/3/discover/movie")
+            comps?.queryItems = [
+                URLQueryItem(name: "include_adult", value: "false"),
+                URLQueryItem(name: "language", value: "en-US"),
+                URLQueryItem(name: "page", value: String(page))
+            ]
+        }
+        
+        guard let url = comps?.url else {
+            throw URLError(.badURL)
+        }
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWU0YjMyYWIzN2U1MDg2ZmU1YzA5NTIxYzBlNjdhNyIsInN1YiI6IjU2NTYzOTFlOTI1MTQxMDllODAwMDMyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CnjFlQK6eKafVglC_aN3jx98dn9TD_SulgMz86RGohw"
+        ]
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        return try decoder.decode(Response.self, from: data)
+    }
+    
+}
